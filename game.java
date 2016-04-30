27de4d1eceed5b282c555ee2b2b7c6af80ba5a82
fkurniaw <FKURNIAW@uwaterloo.ca>
2016-04-30 16:30:42 -0400

    Create game.java
    Game screen (very far from being finished)

---
+++ game.java
@@ -0,0 +1,63 @@
+package com.example.felix.myfirstapp;
+
+import android.annotation.TargetApi;
+import android.content.ClipData;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.felix.myfirstapp.R;
+
+@TargetApi(11)
+public class game extends AppCompatActivity {
+
+    Button firstaid;
+    Button smoothieButton;
+    Button pizzaButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        firstaid = (Button) findViewById(R.id.firstaid);
+        firstaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(game.this, firstaid.class);
+                startActivity(i);
+            }
+        });
+
+       smoothieButton = (Button) findViewById(R.id.smoothieButton);
+       smoothieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(game.this, burgerPopUp.class);
+                startActivity(i);
+            }
+        });
+
+        pizzaButton = (Button) findViewById(R.id.pizzaButton);
+        pizzaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(game.this, burgerPopUp.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
